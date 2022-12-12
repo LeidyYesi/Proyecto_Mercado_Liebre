3bb0b18b19039d405dac1156c692e73136c88ebe
LaidyYesi <cantamosaleluya@gmail.com>
2022-12-12 10:15:31 -0300

    Formulario y login

--- src/app.js
+++ src/app.js
@@ -18,6 +18,11 @@ app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
   });
 
+  app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+  });
+
+
   // Ponemos a escuchar el servidor
 app.listen(3033, () => {
     console.log("Servidor corriendo en http://localhost:3033")
